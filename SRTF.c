34731d1751a388f6362466474225c32629e47280
Akhelesh Bhandari <akuro01010@gmail.com>
2019-04-01 20:19:19 +0100

    Commit #3

--- SRTF.c
+++ SRTF.c
@@ -9,7 +9,7 @@ int arrivalTime[4] = {0, 0, 2, 5},
     burstTime[4] = {3, 2, 1, 2},
     remainingTime[5] = {3, 2, 1, 2, 1000};
 
-int time, remainingJobs = 4, smallest, i;
+int time, remainingJobs = 4, smallest, i, prevJob = 0;
 
 void *scheduler()
 {
@@ -21,14 +21,23 @@ void *scheduler()
             if (arrivalTime[i] <= time && remainingTime[i] < remainingTime[smallest] && remainingTime[i] > 0)
                 smallest = i;
         }
+        if (prevJob != smallest)
+        {
+            printf("Job #%d: Prempted\n", prevJob);
+            printf("Job #%d: Started\n", smallest);
+        }
         remainingTime[smallest]--;
         if (remainingTime[smallest] == 0)
             remainingJobs--;
+        prevJob = smallest;
     }
+
+    pthread_exit(NULL);
 }
 
 int main()
 {
     pthread_t schedule_thread;
     pthread_create(&schedule_thread, NULL, &scheduler, NULL);
+    pthread_join(schedule_thread, NULL);
 }
