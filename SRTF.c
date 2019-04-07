86ff49d2de9fb3286c9ea59d4f516476422df403
Akhelesh Bhandari <akuro01010@gmail.com>
2019-04-07 13:02:30 +0530

    Commit #5

--- SRTF.c
+++ SRTF.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#define rep(i, l, u) for (int i = l; i < u; i++)
 #define print(m, n) printf("%s: %d\n", m, n)
-#define printA()                         \
+#define printRemaingTime()               \
+    printf("Remaining Time: ");          \
     for (int i = 0; i < 5; i++)          \
     {                                    \
         printf("%d ", remainingTime[i]); \
@@ -12,7 +12,6 @@
     printf("\n");
 
 int arrivalTime[4] = {0, 0, 2, 5},
-    burstTime[4] = {3, 2, 1, 2},
     remainingTime[5] = {3, 2, 1, 2, 1000};
 
 int TIME, remainingJobs = 4, smallest, i, prevJob = 1;
@@ -22,14 +21,12 @@ void *scheduler()
     for (TIME = 0; remainingJobs > 0; TIME++)
     {
         smallest = 4;
-        printA();
+        printRemaingTime();
         for (i = 0; i < 4; i++)
         {
             if ((arrivalTime[i] <= TIME) && (remainingTime[i] < remainingTime[smallest]) && (remainingTime[i] > 0))
                 smallest = i;
-            printf("TIMe -> %d Smallest ###### %d\n", TIME, smallest);
         }
-        print("SMALLEST-> ", smallest);
         // if (remainingTime[prevJob] == remainingTime[smallest])
         // smallest = prevJob;
         if (prevJob != smallest)
@@ -38,13 +35,11 @@ void *scheduler()
             printf("TIME: %d Job #%d: START\n", TIME, smallest);
         }
         remainingTime[smallest]--;
-        printf("TIME %d RT: Job %d T %d\n", TIME, smallest, remainingTime[smallest]);
         // printA();
         if (remainingTime[smallest] == 0)
         {
             remainingJobs--;
-            printf("RT: Job %d T %d\n", smallest, remainingTime[smallest]);
-            printf("TIME: %d Job %d: END\n", TIME, smallest);
+            printf("TIME: %d Job #%d: END\n", TIME, smallest);
         }
         prevJob = smallest;
     }
